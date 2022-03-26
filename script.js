9d1df968ccec32eeaea1e3129e86ce4d5bab176e
yassine <yassinkaabi14@gmail.com>
2022-03-26 02:56:13 +0100

    Portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: [ "Developer","Front-end","Back-end"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Developer", "Front-end","Back-end"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
