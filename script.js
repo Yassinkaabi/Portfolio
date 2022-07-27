29fdeb205f35101f1a2796fce7d89d9c07d743da
yassine <yassinkaabi14@gmail.com>
2022-07-27 03:05:06 +0200

    change image

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
-
+    // // toggle menu/navbar script
+    // $('.menu-btn').click(function(){
+    //     $(' header  .navigation').toggleClass("active");
+    //     $('.menu-btn i').toggleClass("active");
+    // });
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [ "Developer","Front-end","Back-end"],
+        strings: [ "Web Developer","Front-end","Back-end"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Front-end","Back-end"],
+        strings: ["Web Developer", "Front-end","Back-end"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
